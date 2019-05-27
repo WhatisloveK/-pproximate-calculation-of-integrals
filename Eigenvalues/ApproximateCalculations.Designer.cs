9a52ad7e8b511494d76cf37f787b9797c721e0c3
Каплюк Владислав Олегович <kuplykvlad@gmail.com>
2019-05-27 20:19:31 +0300

    Add project files.

---
+++ Eigenvalues/ApproximateCalculations.Designer.cs
@@ -0,0 +1,258 @@
+﻿namespace Eigenvalues
+{
+    partial class ApproximateCalculations
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.label1 = new System.Windows.Forms.Label();
+            this.numericUpDownRows = new System.Windows.Forms.NumericUpDown();
+            this.buttonSize = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.radioButtonSqure = new System.Windows.Forms.RadioButton();
+            this.radioButtonTrapeze = new System.Windows.Forms.RadioButton();
+            this.numericUpDownSegments = new System.Windows.Forms.NumericUpDown();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.numericUpDownA = new System.Windows.Forms.NumericUpDown();
+            this.numericUpDownB = new System.Windows.Forms.NumericUpDown();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDownRows)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDownSegments)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDownA)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDownB)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(12, 219);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(482, 150);
+            this.dataGridView1.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(38, 89);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(118, 16);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Polinomial degree";
+            // 
+            // numericUpDownRows
+            // 
+            this.numericUpDownRows.Location = new System.Drawing.Point(178, 85);
+            this.numericUpDownRows.Minimum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.numericUpDownRows.Name = "numericUpDownRows";
+            this.numericUpDownRows.Size = new System.Drawing.Size(37, 20);
+            this.numericUpDownRows.TabIndex = 2;
+            this.numericUpDownRows.Value = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            // 
+            // buttonSize
+            // 
+            this.buttonSize.Location = new System.Drawing.Point(264, 38);
+            this.buttonSize.Name = "buttonSize";
+            this.buttonSize.Size = new System.Drawing.Size(51, 23);
+            this.buttonSize.TabIndex = 4;
+            this.buttonSize.Text = "Apply";
+            this.buttonSize.UseVisualStyleBackColor = true;
+            this.buttonSize.Click += new System.EventHandler(this.buttonSize_Click);
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(355, 38);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(133, 67);
+            this.button1.TabIndex = 5;
+            this.button1.Text = "Integrate me!";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(12, 112);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(0, 16);
+            this.label2.TabIndex = 10;
+            // 
+            // radioButtonSqure
+            // 
+            this.radioButtonSqure.AutoSize = true;
+            this.radioButtonSqure.Location = new System.Drawing.Point(31, 13);
+            this.radioButtonSqure.Name = "radioButtonSqure";
+            this.radioButtonSqure.Size = new System.Drawing.Size(184, 17);
+            this.radioButtonSqure.TabIndex = 11;
+            this.radioButtonSqure.TabStop = true;
+            this.radioButtonSqure.Text = "Formula of the middle of the squre";
+            this.radioButtonSqure.UseVisualStyleBackColor = true;
+            // 
+            // radioButtonTrapeze
+            // 
+            this.radioButtonTrapeze.AutoSize = true;
+            this.radioButtonTrapeze.Location = new System.Drawing.Point(31, 44);
+            this.radioButtonTrapeze.Name = "radioButtonTrapeze";
+            this.radioButtonTrapeze.Size = new System.Drawing.Size(168, 17);
+            this.radioButtonTrapeze.TabIndex = 12;
+            this.radioButtonTrapeze.TabStop = true;
+            this.radioButtonTrapeze.Text = "Formula of the trapeze method";
+            this.radioButtonTrapeze.UseVisualStyleBackColor = true;
+            // 
+            // numericUpDownSegments
+            // 
+            this.numericUpDownSegments.Location = new System.Drawing.Point(178, 122);
+            this.numericUpDownSegments.Minimum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.numericUpDownSegments.Name = "numericUpDownSegments";
+            this.numericUpDownSegments.Size = new System.Drawing.Size(37, 20);
+            this.numericUpDownSegments.TabIndex = 13;
+            this.numericUpDownSegments.Value = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(40, 126);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(132, 16);
+            this.label3.TabIndex = 14;
+            this.label3.Text = "Number of segments";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(264, 126);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(51, 16);
+            this.label4.TabIndex = 15;
+            this.label4.Text = "Interval";
+            // 
+            // numericUpDownA
+            // 
+            this.numericUpDownA.Location = new System.Drawing.Point(321, 126);
+            this.numericUpDownA.Minimum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.numericUpDownA.Name = "numericUpDownA";
+            this.numericUpDownA.Size = new System.Drawing.Size(37, 20);
+            this.numericUpDownA.TabIndex = 16;
+            this.numericUpDownA.Value = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            // 
+            // numericUpDownB
+            // 
+            this.numericUpDownB.Location = new System.Drawing.Point(364, 126);
+            this.numericUpDownB.Minimum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.numericUpDownB.Name = "numericUpDownB";
+            this.numericUpDownB.Size = new System.Drawing.Size(37, 20);
+            this.numericUpDownB.TabIndex = 17;
+            this.numericUpDownB.Value = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            // 
+            // ApproximateCalculations
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(506, 381);
+            this.Controls.Add(this.numericUpDownB);
+            this.Controls.Add(this.numericUpDownA);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.numericUpDownSegments);
+            this.Controls.Add(this.radioButtonTrapeze);
+            this.Controls.Add(this.radioButtonSqure);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.buttonSize);
+            this.Controls.Add(this.numericUpDownRows);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.dataGridView1);
+            this.Name = "ApproximateCalculations";
+            this.Text = "Approximate integrals calculation";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDownRows)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDownSegments)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDownA)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDownB)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.NumericUpDown numericUpDownRows;
+        private System.Windows.Forms.Button buttonSize;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.RadioButton radioButtonSqure;
+        private System.Windows.Forms.RadioButton radioButtonTrapeze;
+        private System.Windows.Forms.NumericUpDown numericUpDownSegments;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.NumericUpDown numericUpDownA;
+        private System.Windows.Forms.NumericUpDown numericUpDownB;
+    }
+}
+
---
+++ Eigenvalues/ApproximateCalculations.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using MathNet.Numerics.LinearAlgebra;
+namespace Eigenvalues
+{
+    public partial class ApproximateCalculations : Form
+    {
+        
+      
+        public ApproximateCalculations()
+        {
+            InitializeComponent();
+            InitializeDataGridView(2, 2);
+        }
+        private void InitializeDataGridView(int rows, int columns)
+        {
+            dataGridView1.ColumnCount = columns;
+            dataGridView1.ColumnHeadersVisible = true;
+
+            DataGridViewCellStyle columnHeaderStyle = new DataGridViewCellStyle();
+            columnHeaderStyle.BackColor = Color.Beige;
+            columnHeaderStyle.Font = new Font("Times New Roman", 12, FontStyle.Bold);
+            dataGridView1.ColumnHeadersDefaultCellStyle = columnHeaderStyle;
+
+
+            for (int i = 0; i < columns; ++i)
+            {
+                dataGridView1.Columns[i].Width = 64;
+                dataGridView1.Columns[i].Name = (i + 1).ToString();
+            }
+
+            dataGridView1.RowCount = rows + 2;
+            dataGridView1.RowHeadersVisible = true;
+
+            DataGridViewCellStyle rowHeaderStyle = new DataGridViewCellStyle();
+            rowHeaderStyle.BackColor = Color.Beige;
+            rowHeaderStyle.Font = new Font("Times New Roman", 12, FontStyle.Bold);
+            dataGridView1.RowHeadersDefaultCellStyle = rowHeaderStyle;
+
+            //f = new Class26BasedSys();
+            
+            dataGridView1.Rows[0].HeaderCell.Value = "X";
+            dataGridView1.Rows[0].ReadOnly = false;
+            
+            dataGridView1.Rows[rows-1].HeaderCell.Value = "";
+            dataGridView1.Rows[rows-1].ReadOnly = true;
+            dataGridView1.Rows[rows].HeaderCell.Value = "F";
+
+            dataGridView1.AutoResizeRowHeadersWidth(DataGridViewRowHeadersWidthSizeMode.AutoSizeToAllHeaders);
+            dataGridView1.RowHeadersWidth = 64;
+            
+        }
+
+        private void buttonSize_Click(object sender, EventArgs e)
+        {
+            InitializeDataGridView(2, (int)numericUpDownRows.Value);
+        }
+
+
+
+       
+
+        private double ExecuteSqure(double[] list)
+        {
+            int n = (int)numericUpDownSegments.Value;
+            
+            return;
+        }
+        private double ExecuteTrapeze(double[] list)
+        {
+            return;
+        }
+
+        
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            double[] polinomialCoeff = new double[(int)numericUpDownRows.Value];
+            
+            if (!radioButtonSqure.Checked)
+            {
+                for (int j = 0; j < dataGridView1.ColumnCount; j++)
+                {
+                    polinomialCoeff[j] = Double.Parse(dataGridView1.Rows[0].Cells[j].Value.ToString());
+                }
+            }
+            else
+            {
+                for (int j = 0; j < dataGridView1.ColumnCount; j++)
+                {
+                    //listX[j] = ChebyshovPolinomial(j,dataGridView1.ColumnCount);
+                    //listF[j] = Double.Parse(dataGridView1.Rows[2].Cells[j].Value.ToString());
+                }
+            }
+            double[] denominator = ExecuteSqure(polinomialCoeff);
+            string res = "";
+            for(int i = 0; i < listX.Length; i++)
+            {
+                int j = 0;
+                double k = denominator[i] * listF[i];
+                res += ((k > 0 ? ((i != 0) ? '+' : ' ') : '-'));
+                res += Math.Abs(Math.Round(k,4));
+                while (j < listX.Length)
+                {
+                    if (i != j)
+                    {
+                        res += "(x " + (listX[j]>0 ? '-' : '+')+" "+Math.Abs(Math.Round(listX[j],4)) + ")";
+                    }
+                    j++;
+                }
+                res += "\n";
+            }
+            label2.Text = res;
+        }
+    }
+}
